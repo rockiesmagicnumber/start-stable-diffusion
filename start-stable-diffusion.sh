b0dc3206d7333f2a97ad040baad34c65bade782b
Andrew Martin <rockiesmagicnumber@gmail.com>
2022-10-02 20:14:46 -0600

    dream is working; need to test args passing to txt2img and img2img

--- start-stable-diffusion.sh
+++ start-stable-diffusion.sh
@@ -1,19 +1,78 @@
 #!/bin/bash
-# this is gonna run a bunch of shit so I don't have to type... wait... type... wait... when launching stable diffusion
 
+# functions	
+activateLdm(){
+	echo activateLdm
+	conda activate ldm
+	echo ldm Activated
+}
 
-echo "Entering stable-diffusion directory"
-cd ~/stable-diffusion
+setCondaPath(){	
+	echo setCondaPath
+	CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
+	source $CONDA_PATH/etc/profile.d/conda.sh
+	echo Conda Path Set
+}
 
-CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
-source $CONDA_PATH/etc/profile.d/conda.sh
-conda activate $MY_ENV
+preloadModels(){
+	 echo preloadModels
+	 python3 "$HOME/stable-diffusion/dream/scripts/preload_models.py"
+	 echo Models preloaded
+}
 
-echo "activating ldm via conda."
-conda activate ldm
+dream(){
+	echo dream
+	cd "$HOME/stable-diffusion/dream"
+ 	python3 "$HOME/stable-diffusion/dream/scripts/dream.py"
+ 	echo awaken
+}
 
-echo "preloading the stable-diffusion content"
-python3 scripts/preload_models.py
+bootstrap(){
+	setCondaPath
+	activateLdm
+}
 
-echo "let's get cracking"
-python3 scripts/dream.py
+executetxt2img(){
+	echo runtxt2img
+	python "$HOME/stable-diffusion/optimized/optimizedSD/optimized_txt2img.py \"$@\""
+	echo txt2img run complete
+}
+
+executeimg2img(){
+	echo runimg2img
+	python "$HOME/stable-diffusion/optimized/optimizedSD/optimized_img2img.py \"$@\""
+	echo img2img run complete
+}
+
+runtxt2img(){
+	executetxt2img "$@"
+}
+
+runimg2img(){
+	executeimg2img "$@"
+}
+
+rundream(){
+	preloadModels
+	dream
+}
+
+# execution
+bootstrap
+allArgs="$@"
+
+echo "Select an action: "
+echo "1) txt2img"
+echo "2) img2img"
+echo "3) dream"
+echo "4) quit (also q)"
+
+read action
+
+case $action in
+	1) runtxt2img allArgs;;
+	2) runimg2img allArgs;;
+	3) rundream;;
+	4) exit;;
+	q) exit;;
+esac
